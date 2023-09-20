f16c7a406bded4f737f7378dd33c5f8ba9d07614
HerrBergenholtz <zbdv8800@gmail.com>
2023-09-20 10:43:00 +0200

    Add project files.

---
+++ Uppgift 2.5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Uppgift_2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in en multiplikation av två decimaltal på raden nedan");
+            string multi = Console.ReadLine();
+            int tecken = multi.IndexOf("*");
+            string tal1Text = multi[..tecken];
+            string tal2Text = multi[(tecken + 1)..];
+
+            float tal1 = float.Parse(tal1Text);
+            float tal2 = float.Parse(tal2Text);
+            float summa = tal1 * tal2;
+
+            Console.WriteLine(multi + "=" + summa);
+
+            Console.ReadKey();
+        }
+    }
+}
